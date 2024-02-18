62d329f9a4e563c305434d426764bc0b7655d276
Paulasalvo <p.arroyosalvo@gmail.com>
2024-02-17 20:30:43 -0800

    Add modifier, theme and surface

--- app/src/main/java/com/namkuzo/androidcourse/MainActivity.kt
+++ app/src/main/java/com/namkuzo/androidcourse/MainActivity.kt
@@ -3,14 +3,23 @@ package com.namkuzo.androidcourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
 import com.namkuzo.androidcourse.data.Message
+import com.namkuzo.androidcourse.ui.theme.AndroidCourseTheme
 import com.namkuzo.androidcourse.ui.theme.component.MessageCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            AndroidCourseTheme {
+                Surface {
+                    MessageCard(
+                        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+                    )
+                }
+
+            }
         }
     }
 }
--- app/src/main/java/com/namkuzo/androidcourse/ui/theme/component/MessageCard.kt
+++ app/src/main/java/com/namkuzo/androidcourse/ui/theme/component/MessageCard.kt
@@ -1,6 +1,8 @@
 package com.namkuzo.androidcourse.ui.theme.component
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +10,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,6 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.namkuzo.androidcourse.R
 import com.namkuzo.androidcourse.data.Message
+import com.namkuzo.androidcourse.ui.theme.AndroidCourseTheme
 
 @Composable
 fun MessageCard(msg: Message) {
@@ -24,25 +29,44 @@ fun MessageCard(msg: Message) {
         Image(
             modifier= Modifier
                 .width(100.dp)
-                .clip(CircleShape),
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape),
             painter = painterResource(id = R.drawable.girl),
             contentDescription ="ita a pretty girl"
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall)
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(modifier = Modifier.padding(4.dp),
+                    text = msg.body,
+                    style = MaterialTheme.typography.bodyMedium)
+            }
+
         }
     }
 
 
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+    AndroidCourseTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+            )
+        }
+
+    }
+
 }
