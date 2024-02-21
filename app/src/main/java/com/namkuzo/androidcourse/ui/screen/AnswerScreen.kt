dc7d25179b4394f71ccdeca9e91e9feb98521250
Paulasalvo <p.arroyosalvo@gmail.com>
2024-02-20 16:45:59 -0800

    add background spacer

--- app/src/main/java/com/namkuzo/androidcourse/ui/screen/AnswerScreen.kt
+++ app/src/main/java/com/namkuzo/androidcourse/ui/screen/AnswerScreen.kt
@@ -2,10 +2,13 @@ package com.namkuzo.androidcourse.ui.screen
 
 import SampleData
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,7 +44,11 @@ fun AnswerScreen(answerList: List<Answer>) {
                     Toast.makeText(context, it.text, Toast.LENGTH_SHORT).show()
                 }
             )
-            Spacer(modifier = Modifier.padding(top = 8.dp))
+            Spacer(modifier = Modifier
+                .padding(top = 8.dp)
+                .background(Color.Red)
+                .height(2.dp))
+
         }
     }
 }
