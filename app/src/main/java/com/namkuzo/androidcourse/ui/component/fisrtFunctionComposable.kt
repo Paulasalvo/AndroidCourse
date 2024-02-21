286cf80f0f0363028fcd2b1c199cf91c58739f70
Paulasalvo <p.arroyosalvo@gmail.com>
2024-02-21 15:23:17 -0800

    add function composable

---
+++ app/src/main/java/com/namkuzo/androidcourse/ui/component/fisrtFunctionComposable.kt
@@ -0,0 +1,31 @@
+package com.namkuzo.androidcourse.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.namkuzo.androidcourse.ui.theme.AndroidCourseTheme
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidCourseTheme {
+        Greeting("Paly")
+    }
+}
