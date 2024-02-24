185aedf6dd4a6d5b848dc26b8e64cabbb60c5187
Paulasalvo <p.arroyosalvo@gmail.com>
2024-02-23 18:57:57 -0800

    add ElevatedButton

--- app/src/main/java/com/namkuzo/androidcourse/ui/component/fisrtFunctionComposable.kt
+++ app/src/main/java/com/namkuzo/androidcourse/ui/component/fisrtFunctionComposable.kt
@@ -2,13 +2,17 @@ package com.namkuzo.androidcourse.ui.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,16 +21,28 @@ import com.namkuzo.androidcourse.ui.theme.AndroidCourseTheme
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-   Surface(
-       modifier.padding(horizontal = 8.dp, vertical = 4.dp),
-       color = MaterialTheme.colorScheme.primary
-   ) {
-       Column (modifier = modifier.fillMaxWidth().padding(24.dp)) {
-           Text(text = "Hello")
-           Text(text = "$name!")
+    val expanded = remember { mutableStateOf(false)}
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+        Surface(
+            modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+            color = MaterialTheme.colorScheme.primary
+        ) {
+            Row(modifier = Modifier.padding(24.dp)) {
+                Column(modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)) {
+                    Text(text = "Hello ")
+                    Text(text = name)
+                }
+                ElevatedButton(
+                    onClick = { expanded.value = !expanded.value },
+                ) {
+                    Text(if (expanded.value) "Show less" else "Show more")
+                }
+            }
         }
     }
-}
+
 
 @Composable
 fun MyApp(
