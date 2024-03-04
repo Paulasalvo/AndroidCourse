02df9436b06935e223e23f24bb819812ebad6e25
Paulasalvo <p.arroyosalvo@gmail.com>
2024-03-03 17:45:51 -0800

    State driven in (add WaterCounter function)

--- app/src/main/java/com/namkuzo/androidcourse/MainActivity.kt
+++ app/src/main/java/com/namkuzo/androidcourse/MainActivity.kt
@@ -3,17 +3,29 @@ package com.namkuzo.androidcourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.ui.Modifier
 import com.namkuzo.androidcourse.ui.component.MySootheApp
+import com.namkuzo.androidcourse.ui.component.state.WellnessScreen
+import com.namkuzo.androidcourse.ui.theme.AndroidCourseTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                val windowSizeClass = calculateWindowSizeClass(this)
-                MySootheApp(windowSizeClass)
+                    AndroidCourseTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color=MaterialTheme.colorScheme.background
+                        ) {
+                            WellnessScreen()
+                        }
+                    }
         }
     }
 }
